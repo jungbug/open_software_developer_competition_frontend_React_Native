c241500828c51748a5b68d361833b75543a34019
kosw <swko4512@gmail.com>
2023-07-17 22:15:41 +0900

    test

--- src/People/People.js
+++ src/People/People.js
@@ -1,26 +1,42 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { Camera } from 'expo-camera';
 
 export default function People() {
   const cameraRef = useRef(null);
+  const [hasPermission, setHasPermission] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
 
   const takePhoto = async () => {
     if (cameraRef.current) {
-      const photo = await cameraRef.current.takePictureAsync();//여기 변수에 사진이 저장됨
+      const photo = await cameraRef.current.takePictureAsync();
 
       // 사진 캡처 후 처리할 작업 수행
 
-      console.log(photo); // 콘솔에 사진 데이터 출력
+      console.log(photo);
     }
   };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
 
   return (
     <View style={{ flex: 1 }}>
       <Camera
         ref={cameraRef}
-        style={{ flex: 1, aspectRatio: 4 / 3 }} // 화면 비율을 4:3으로 조정
-        type={Camera.Constants.Type.back} // 후면 카메라 사용
+        style={{ flex: 1, aspectRatio: 4 / 3 }}
+        type={Camera.Constants.Type.back}
       >
         <View style={styles.buttonContainer}>
           <TouchableOpacity onPress={takePhoto} style={styles.captureButton}>
