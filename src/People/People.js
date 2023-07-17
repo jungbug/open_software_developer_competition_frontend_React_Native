e28ae990eb9ff79fc6e46341df1b51e019451b06
kojs <js95112345@gmail.com>
2023-07-17 13:00:21 +0900

    feat: Upload videos

--- src/People/People.js
+++ src/People/People.js
@@ -1,39 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { View, Text, Pressable } from 'react-native';
-import * as ImagePicker from "expo-image-picker";
+
 
 export default function People() {
-  const ImagePickerComponent = () => {
-    const [imageUrl, setImageUrl] = useState('');
-    const [status, requestPermission] = ImagePicker.useMediaLibraryPermissions();
-
-    const uploadImage = async () => {
-      if (!status?.granted) {
-        const permission = await requestPermission();
-        if (!permission.granted) {
-          return null;
-        }
-      }
-      const result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-        allowsEditing: false,
-        quality: 1,
-        aspect: [1, 1]
-      });
-      if (result.cancelled) {
-        return null;
-      }
-
-      console.log(result);
-      setImageUrl(result.uri);
-    }
-
-    return (
-      <Pressable onPress={uploadImage}>
-        <Text>이미지 업로드하기</Text>
-      </Pressable>
-    );
-  };
-
-  return <ImagePickerComponent />;
+  return (
+    <View>
+      <Text>Welcome to the People page!</Text>
+    </View>
+  );
 }
--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Pressable } from 'react-native';
+import * as ImagePicker from "expo-image-picker";
 
-export default function Settings() {
-  return (
-    <View>
-      <Text>Welcome to the Settings page!</Text>
-    </View>
-  );
+export default function People() {
+  const ImagePickerComponent = () => {
+    const [imageUrl, setImageUrl] = useState('');
+    const [status, requestPermission] = ImagePicker.useMediaLibraryPermissions();
+
+    const uploadImage = async () => {
+      if (!status?.granted) {
+        const permission = await requestPermission();
+        if (!permission.granted) {
+          return null;
+        }
+      }
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+        allowsEditing: false,
+        quality: 1,
+        aspect: [1, 1]
+      });
+      if (result.cancelled) {
+        return null;
+      }
+
+      console.log(result);
+      setImageUrl(result.uri);
+    }
+
+    return (
+      <Pressable onPress={uploadImage}>
+        <Text>동영상 업로드하기</Text>
+      </Pressable>
+    );
+  };
+
+  return <ImagePickerComponent />;
 }
