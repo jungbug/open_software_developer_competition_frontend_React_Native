1123d8303e6e3871260191f61f19e2691ba35887
kosw <swko4512@gmail.com>
2023-07-20 18:02:50 +0900

    style : More.js text style fix

--- src/More/More.js
+++ src/More/More.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default function More() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
+        <Icon name="chevron-back-outline" size={24} color="#000" />
+        <Text style={styles.headerText}>홈</Text>
+        <Text style={styles.headerText}>                              </Text>
         <Text style={styles.headerText}>추천식단</Text>
+        <Text style={styles.headerText}>                                        </Text>
       </View>
 
       <View style={styles.middle}>
@@ -26,23 +31,25 @@ const styles = StyleSheet.create({
   },
   header: {
     flex: 0.7,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f2f',
   },
   headerText: {
     fontSize: 16,
     fontWeight: 'bold',
   },
   middleText: {
-    fontSize: 24,
+    fontSize: 30,
     fontWeight: 'bold',
+    color: '#9370DB',
+    marginTop: -50,
+    marginLeft: -220,
   },
   middle: {
-    flex: 0.5,
+    flex: 0.1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f21',
   },
   last: {
     flex: 3,
