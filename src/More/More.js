ec7722dc3dced7bcce749dbf8da7fa8f46e84d28
kojs <js95112345@gmail.com>
2023-07-21 23:19:26 +0900

    feat: 추천식단 문구 추가

--- src/More/More.js
+++ src/More/More.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+
 export default function More() {
+  //const NUTRIENTS = [90, 10, 10, 10, 10];
+  //const NUTRIENTS1 = [protein, bitamin, mineral, fiber, carbon];
+
+  //const cal = 30;//칼로리
+
+  const recList = [rec1 = "닭가슴살", rec2 = "샐러드", rec3 = "현미밥", rec4 = "삶은 고구마", rec5 = "삶은 계란"]; // 추천 식단 목록
+
+
+  /*const proteinCal = () => {
+    if (NUTRIENTS[0] > 50 && cal < 50) {
+      recList[0] = "닭가슴살"
+    }
+
+  }*/
+
+  const recExList = ["닭가슴살은 칼로리가 낮지만 고단백질입니다.", "샐러드는 비타민 및 무기질, 섬유질이 많고 포만감이 높습니다",
+    "백미밥의 환경과는 달리, 쌀겨와 쌀눈에 있는 영양소를 섭취할 수 있게 됩니다 ...(설명)", "추천운동과 진행하며 지방을 에너지로...(설명)",
+    "삶은 계란은 단백질 함량이 많기 때문에...(설명)"] // 식단 설명
+
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -19,6 +42,26 @@ export default function More() {
 
       {/* 마지막 부분 */}
       <View style={styles.last}>
+        <View style={styles.recommendFood}>
+          <Text style={styles.foodName}>{rec1}</Text>
+          <Text style={styles.foodDetail}>{recExList[0]}</Text>
+        </View>
+        <View style={styles.recommendFood}>
+          <Text style={styles.foodName}>{rec2}</Text>
+          <Text style={styles.foodDetail}>{recExList[1]}</Text>
+        </View>
+        <View style={styles.recommendFood}>
+          <Text style={styles.foodName}>{rec3}</Text>
+          <Text style={styles.foodDetail}>{recExList[2]}</Text>
+        </View>
+        <View style={styles.recommendFood}>
+          <Text style={styles.foodName}>{rec4}</Text>
+          <Text style={styles.foodDetail}>{recExList[3]}</Text>
+        </View>
+        <View style={styles.recommendFood}>
+          <Text style={styles.foodName}>{rec5}</Text>
+          <Text style={styles.foodDetail}>{recExList[4]}</Text>
+        </View>
         {/* 마지막 부분 내용을 채워주세요 */}
       </View>
     </View>
@@ -56,5 +99,35 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#f111',
+    borderRadius: 20,
+  },
+  recommendFood: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  foodName: {
+    fontSize: 40,
+    color: 'blue',
+    marginLeft: 60,
+    marginRight: 60,
+  },
+  foodDetail: {
+    fontSize: 15,
+    color: 'black',
+    marginLeft: 60,
+    marginRight: 60,
   },
 });
+
+/*function generateAllCombinations() {
+  const variables = ['protein', 'vitamin', 'mineral', 'fiber', 'carbohydrate'];
+  const totalCombinations = 2 ** variables.length; // 2의 n승 (n은 변수의 개수)
+
+  for (let i = 0; i < totalCombinations; i++) {
+    const combination = {};
+    for (let j = 0; j < variables.length; j++) {
+      const variableValue = (i & (1 << j)) !== 0;
+      combination[variables[j]] = variableValue;
+    }
+  }
+}*/
