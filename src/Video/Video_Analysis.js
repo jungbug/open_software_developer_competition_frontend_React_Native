25635e3eda1eea022b1b2ebde984d286e6a1fbad
kojs <js95112345@gmail.com>
2023-07-23 23:42:40 +0900

    자세교정화면 프레임

--- src/Video/Video_Analysis.js
+++ src/Video/Video_Analysis.js
@@ -1,10 +1,53 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Image, Text, StyleSheet, Dimensions } from 'react-native';
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function Video_Analysis() {
   return (
     <View>
-      <Text>1</Text>
+      <Image
+        source={require('../../assets/Plank.jpg')}
+        style={styles.imageE} />
+
+      <Text style={styles.correction}>
+        교정사항칸
+      </Text>
+      <Text style={styles.manual}>
+        메뉴얼칸
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  imageE: {
+    flex: 1,
+    width: SCREEN_HEIGHT / 2.4,
+    borderRadius: 20,
+  },
+  correction: {
+    flex: 1,
+    margin: 20,
+
+  },
+  correctionMain: {
+
+  },
+  correctionSub: {
+
+  },
+  manual: {
+    flex: 1,
+    margin: 20,
+  },
+  manualMain: {
+
+  },
+  manualSub: {
+
+  },
+
+
+});
