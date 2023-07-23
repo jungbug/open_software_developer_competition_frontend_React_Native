a79e04dca62e84bcfc28d04179f8d89f30e0e604
kosw <swko4512@gmail.com>
2023-07-23 20:26:30 +0900

    feat : 그래프 추가

--- src/Photo/Photo_Analysis.js
+++ src/Photo/Photo_Analysis.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { LineChart } from 'react-native-svg-charts';
 
 const FOOD = '샐러드';
 
 const Photo_Analysis = ({ onNavigateToPhoto }) => {
+  const lineChartData = [10, 20, 15, 30];
+
   return (
-    <ScrollView contentContainerStyle={styles.container}>
+    <ScrollView contentContainerStyle={styles.container} showsVerticalScrollIndicator={false}>
       <View style={styles.firstContainer}>
-      <TouchableOpacity onPress={onNavigateToPhoto}>
-        <Text style={styles.text}>카메라</Text>
-      </TouchableOpacity>
-        <Text style={styles.text}>                         식품분석                                     </Text>
+        <TouchableOpacity onPress={onNavigateToPhoto}>
+          <Text style={styles.text}>카메라</Text>
+        </TouchableOpacity>
+        <Text style={styles.text}>                        식품분석                                   </Text>
       </View>
 
       <View style={styles.secondContainer}>
         <Text style={styles.middleText}>{FOOD}</Text>
       </View>
 
-      <View style={styles.thirdContainer}></View>
+      {/* Third Container - Line Chart */}
+      <View style={styles.thirdContainer}>
+        <LineChart
+          data={lineChartData}
+          style={{ flex: 1 }}
+          contentInset={{ top: 20, bottom: 20 }}
+          svg={{ stroke: 'rgba(134, 65, 244, 1)', strokeWidth: 2 }}
+        />
+      </View>
 
       <View style={styles.fourthContainer}>
         <Text style={styles.secondmiddleText}>주간 분석</Text>
@@ -27,26 +38,23 @@ const Photo_Analysis = ({ onNavigateToPhoto }) => {
     </ScrollView>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
-    paddingVertical: 20,
+    paddingVertical: 50,
   },
   firstContainer: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
-    marginBottom: 10,
   },
   secondContainer: {
     flex: 1,
-    marginBottom: 10,
+    marginTop: 30,
   },
   thirdContainer: {
     flex: 4,
-    backgroundColor: 'blue',
     marginBottom: 10,
   },
   fourthContainer: {
