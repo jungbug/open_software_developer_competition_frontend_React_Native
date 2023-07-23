ffca7dd61c8a65de1cedcaf7491df835781a0606
kosw <swko4512@gmail.com>
2023-07-23 17:38:24 +0900

    feat : 화면 분활

--- src/Photo/Photo_Analysis.js
+++ src/Photo/Photo_Analysis.js
@@ -1,10 +1,60 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 
 export default function Photo_Analysis() {
   return (
-    <View>
-      <Text>2</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.firstContainer}>
+        <Text style={styles.text}>카메라</Text>
+        <Text style={styles.text}>식품분석</Text>
+      </View>
+
+      <View style={styles.secondContainer}></View>
+
+      <View style={styles.thirdContainer}></View>
+
+      <View style={styles.fourthContainer}></View>
+
+      <View style={styles.fifthContainer}></View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    paddingVertical: 20,
+  },
+  firstContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: 'red',
+    marginBottom: 10,
+  },
+  secondContainer: {
+    flex: 1,
+    backgroundColor: 'green',
+    marginBottom: 10,
+  },
+  thirdContainer: {
+    flex: 4,
+    backgroundColor: 'blue',
+    marginBottom: 10,
+  },
+  fourthContainer: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    marginBottom: 10,
+  },
+  fifthContainer: {
+    flex: 4,
+    backgroundColor: 'orange',
+    marginBottom: 10,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
