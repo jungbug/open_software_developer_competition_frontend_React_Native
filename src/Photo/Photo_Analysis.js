8e7ceeb9b8e27919819d700e96a9cbc8cbdcb0af
kosw <swko4512@gmail.com>
2023-07-30 19:06:36 +0900

    test

--- src/Photo/Photo_Analysis.js
+++ src/Photo/Photo_Analysis.js
@@ -5,6 +5,7 @@ const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 const FOOD = '샐러드';
 
+
 const Photo_Analysis = ({ onNavigateToPhoto }) => {
 
   const proteinData = [1, 2, 3, 4]; // 샘플데이터
