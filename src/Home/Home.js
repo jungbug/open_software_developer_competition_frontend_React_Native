1df3c7bf7191a869b7c904f03d698888e503998c
kosw <swko4512@gmail.com>
2023-09-06 11:18:58 +0900

    test

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity, Dimensions, } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity, Dimensions } from 'react-native';
 import { BarChart, XAxis } from 'react-native-svg-charts';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { api_uri } from '@env';
 
-const { height: SCREEN_HEIGHT } = Dimensions.get("window");
-const { width: SCREEN_WIDTH } = Dimensions.get("window");
+const { height: SCREEN_HEIGHT, width: SCREEN_WIDTH } = Dimensions.get("window");
 const labels = ['1일차', '2일차', '3일차', '4일차', '5일차', '6일차', '7일차'];
 
-
 const Home = ({ onNavigateToMore, navigateToPhotoAnalysis, navigateToVideoAnalysis }) => {
-  let [nameResult, setNameResult] = useState('');
-  let [accessToken, setAccessToken] = useState('');
+  const [nameResult, setNameResult] = useState('');
   const [weekproteinData, setWeekProteinData] = useState([1, 1, 1, 1]);
+  const [accessToken, setAccessToken] = useState('');
+  const [data, setData] = useState([]);
+
+  let protein = { "닭가슴살": "닭가슴살은 칼로리가 낮지만 고단백질입니다." };
+  let vitamin = { "치즈": "치즈에 들어있는 비타민A와 펩타이드는 항산화 작용으로 암세포 증식을 억제합니다." };
+  let carbon = { "감자": "감자는 탄수화물이 많고 칼로리가 적으며 저렴하게 구할 수 있습니다.", "콩": "콩의 주요 건강 효과에는 체중 감량, 골밀도 증강, 유방암 발병률 감소 등이 있습니다." };
+  let province = { "아몬드": "아몬드는 지방함량이 높지만 적당히 섭취하면 몸에 흡수되지 않으며 포만감을 줍니다." };
+  let minerals = { "양파": "양파에는 많은 무기질이 있어 혈당 조절, 인슐린 생성 촉진, 당뇨 예방에 좋습니다." };
+
+  const NUTRIENTS = [10, 60, 10, 40, 15];
 
   const getData = async () => {
     try {
@@ -25,17 +32,6 @@ const Home = ({ onNavigateToMore, navigateToPhotoAnalysis, navigateToVideoAnalys
       return [null, null];
     }
   };
-  const protein = { "닭가슴살": "닭가슴살은 칼로리가 낮지만 고단백질입니다." };
-  const vitamin = { "치즈": "치즈에 들어있는 비타민A와 펩타이드는 항산화 작용으로 암세포 증식을 억제합니다." };
-  const carbon = { "감자": "감자는 탄수화물이 많고 칼로리가 적으며 저렴하게 구할 수 있습니다.", "콩": "콩의 주요 건강 효과에는 체중 감량, 골밀도 증강, 유방암 발병률 감소 등이 있습니다." };
-  const province = { "아몬드": "아몬드는 지방함량이 높지만 적당히 섭취하면 몸에 흡수되지 않으며 포만감을 줍니다." };
-  const minerals = { "양파": "양파에는 많은 무기질이 있어 혈당 조절, 인슐린 생성 촉진, 당뇨 예방에 좋습니다." };
-
-  const NUTRIENTS = [10, 60, 10, 40, 15];
-  getData().then((result) => {
-    accessToken = result[0];
-    nameResult = result[1];
-  });
 
   useEffect(() => {
     getData().then(([token, name]) => {
@@ -44,102 +40,94 @@ const Home = ({ onNavigateToMore, navigateToPhotoAnalysis, navigateToVideoAnalys
     });
   }, []);
 
+  useEffect(() => {
+    // Fetch and set your chart data here, using your `api_uri` and `accessToken`
+    const fetchChartData = async () => {
+      try {
+        const url = api_uri + '/api/v1/user/nutrient/all';
+        const response = await fetch(url, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + accessToken,
+          },
+        });
+
+        if (response.status === 200) {
+          const responseJson = await response.json();
+          console.log('주간 데이터 가져오기 성공', responseJson);
+
+          if (responseJson.length > 0) {
+            const kcalData = responseJson.map(item => item.kcal);
+            const chartData = kcalData.slice(0, 7);
+            setData(chartData);
+          }
+        } else {
+          console.error('주간 데이터 가져오기 실패:', response.status);
+        }
+      } catch (error) {
+        console.error('An error occurred:', error);
+      }
+    };
+
+    fetchChartData();
+  }, [accessToken]);
+
+  useEffect(() => {
+    getData().then((result) => {
+      accessToken = result[0];
+      nameResult = result[1];
+    });
+  }, []);
+
+  let titleP = "";
+  let subP = "";
+  let titleV = "";
+  let subV = "";
+  let titleC = "";
+  let subC = "";
+  let titlePV = "";
+  let subPV = "";
+  let titleM = "";
+  let subM = "";
+
   if (NUTRIENTS[0] < 50) {
-    proteinR = Math.floor(Math.random() * Object.keys(protein).length)
+    let proteinR = Math.floor(Math.random() * Object.keys(protein).length);
     titleP = Object.keys(protein)[proteinR];
     subP = Object.values(protein)[proteinR];
   } else {
-    titleP = "단백질은 충분합니다!"
-    subP = ""
+    titleP = "단백질은 충분합니다!";
   }
   if (NUTRIENTS[1] < 50) {
-    vitaminR = Math.floor(Math.random() * Object.keys(vitamin).length)
+    let vitaminR = Math.floor(Math.random() * Object.keys(vitamin).length);
     titleV = Object.keys(vitamin)[vitaminR];
     subV = Object.values(vitamin)[vitaminR];
   } else {
-    titleV = "비타민은 충분합니다!"
-    subV = ""
+    titleV = "비타민은 충분합니다!";
   }
   if (NUTRIENTS[2] < 50) {
-    carbonR = Math.floor(Math.random() * Object.keys(carbon).length)
+    let carbonR = Math.floor(Math.random() * Object.keys(carbon).length);
     titleC = Object.keys(carbon)[carbonR];
     subC = Object.values(carbon)[carbonR];
   } else {
-    titleC = "탄수화물은 충분합니다"
-    subC = ""
+    titleC = "탄수화물은 충분합니다";
   }
   if (NUTRIENTS[3] < 50) {
-    provinceR = Math.floor(Math.random() * Object.keys(province).length)
+    let provinceR = Math.floor(Math.random() * Object.keys(province).length);
     titlePV = Object.keys(province)[provinceR];
     subPV = Object.values(province)[provinceR];
   } else {
-    titlePV = "지방은 충분합니다."
-    subPV = ""
+    titlePV = "지방은 충분합니다.";
   }
   if (NUTRIENTS[4] < 50) {
-    mineralsR = Math.floor(Math.random() * Object.keys(minerals).length)
+    let mineralsR = Math.floor(Math.random() * Object.keys(minerals).length);
     titleM = Object.keys(minerals)[mineralsR];
     subM = Object.values(minerals)[mineralsR];
   } else {
-    titleM = "무기질은 충분합니다."
-    subM = ""
+    titleM = "무기질은 충분합니다.";
   }
-  const USERNAME = nameResult
 
-  const data = [10, 20, 30, 40, 50, 60, 70];
-
-  const fetchWeekData = async () => {
-    try {
-      const accessToken = await AsyncStorage.getItem('accessToken');
-
-      if (!accessToken) {
-        console.error('Access token is missing.');
-        return;
-      }
-
-      const url = api_uri + '/api/v1/user/nutrient/weekly';
-      const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-          Authorization: 'Bearer ' + accessToken,
-        },
-      });
-
-      if (response.status === 200) {
-        const responseJson = await response.json();
-        console.log('주간 데이터 가져오기 성공', responseJson);
-
-        if (responseJson.length > 0) {
-          
-          const firstItem = responseJson[0];
-          setWeekProteinData([
-            firstItem.total_kcal,
-            firstItem.total_carbs,
-            firstItem.total_protein,
-            firstItem.total_fat,
-          ]);
-          //아래 코드는 api 엔드포인드가 weekly가 아닌 all일때 사용
-          // setWeekProteinData([
-          //   firstItem.kcal,
-          //   firstItem.carbohydrate,
-          //   firstItem.protein,
-          //   firstItem.fat,
-          // ]);
-        }
-      } else {
-        console.error('주간 데이터 가져오기 실패:', response.status);
-      }
-    } catch (error) {
-      console.error('An error occurred:', error);
-    }
-  };
-
-
-
-  useEffect(() => {
-    fetchWeekData();
-  }, []);
-
+  const USERNAME = nameResult;
 
   return (
     <View Style={styles.container}>
@@ -190,22 +178,22 @@ const Home = ({ onNavigateToMore, navigateToPhotoAnalysis, navigateToVideoAnalys
           </View>
           <Text style={[styles.nut, { color: '#5f4ffe' }]}>   칼로리 주간그래프</Text>
           <BarChart
-            style={styles.chart}
-            data={data}
-            svg={{ fill: '#5f4ffe' }}
-            spacingInner={0.6}
-            spacingOuter={0.1}
-            gridMin={5}
-            animate
-          >
-            <XAxis
-              style={{ marginHorizontal: -5, height: SCREEN_HEIGHT * 0.237, marginTop: SCREEN_HEIGHT * 0.18957346 }}
-              data={data}
-              formatLabel={(value, index) => labels[index]}
-              contentInset={{ left: 20, right: 20 }}
-              svg={{ fontSize: 12, fill: 'black' }}
-            />
-          </BarChart>
+        style={styles.chart}
+        data={data}
+        svg={{ fill: '#5f4ffe' }}
+        spacingInner={0.6}
+        spacingOuter={0.1}
+        gridMin={Math.min(...data)} // Set the gridMin to the minimum value in the data array
+        animate
+      >
+  <XAxis
+    style={{ marginHorizontal: -5, height: SCREEN_HEIGHT * 0.237, marginTop: SCREEN_HEIGHT * 0.18957346 }}
+    data={data}
+    formatLabel={(value, index) => labels[index]}
+    contentInset={{ left: 20, right: 20 }}
+    svg={{ fontSize: 12, fill: 'black' }}
+  />
+</BarChart>
           {/* 밑줄 추가 */}
           <View style={styles.chartFooterLine} />
           <Text style={{ marginTop: SCREEN_HEIGHT * 0.07109, marginLeft: SCREEN_WIDTH * 0.05, fontSize: SCREEN_HEIGHT * SCREEN_WIDTH * 0.000091, fontWeight: "bold", color: '#5f4ffe' }}>추천식단</Text>
